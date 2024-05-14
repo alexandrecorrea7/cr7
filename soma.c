71b216a9ab310a1f6ebe22a38effff04511a767e
alexandrecorrea7 <160781966+alexandrecorrea7@users.noreply.github.com>
2024-05-14 14:05:22 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,16 @@
+void main () {
+int n1,n2,n3,n4,soma;
+printf("n1:");
+scanf("%d",&n1);
+printf("n2:");
+scanf("%d",&n2);
+printf("n3:");
+scanf("%d",&n3);
+printf("n4:");
+scanf("%d",&n4);
+
+soma = n1+n2+n3+n4
+printf ("soma = &d",soma)
+
+
+}
